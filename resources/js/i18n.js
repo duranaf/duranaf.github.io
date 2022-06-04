4061351629cd3fa28c51fa8c17023b30ca51e472
duranaf <ferkys888@azet.sk>
2022-06-04 21:16:20 +0200

    added translations

--- resources/js/i18n.js
+++ resources/js/i18n.js
@@ -37,6 +37,8 @@ $(function () {
               title: 'About me',
               downloadCV: 'Here you can download my CV',
               clickDownload: 'Download CV',
+              aboutProffesions: 'After I graduated from the Faculty of informatics and information technologies at Slovak technical university with a Master\'s degree, I continued improving my skills in the IT-sphere. I collected first real work experiences also during my study and when it was over, I took a path of web development. My favorite part of the application development is building a FrontEnd, UI, and blocks that are responsible for interaction with the end-user of the application also glad to use and incorporate new progressive technologies, that are improving my knowledge.',
+              aboutMe: 'Besides informatics, I am also developing myself in other life\'s aspects. I like to go walking in nature, travel and explore new places. In my free time I also cycle, play guitar or see some interesting movie. I don\'t despise a good book, ideally from a motivation or esoteric genre.'
             },
             education: {
               title: 'Education',
@@ -49,7 +51,8 @@ $(function () {
             },
             jobs: {
               title: 'JOBS',
-              softwareDeveloper: 'Software developer',
+              timeFrame: '12/2020 - Present',
+              softwareDeveloper: 'App developer',
               anasoftAbout: 'Development of a system for BlockChain and Digital signature.',
               softwareArchitecture: 'Software architecture',
               androidDeveloper: 'Android developer',
@@ -57,7 +60,26 @@ $(function () {
               uxDesign: 'UX/UI mobile design',
               userTest: 'Tests with users',
               internationTeam: 'International team',
-              accentureAbout: 'Web applications for wagons management development',
+              accentureAbout: 'Web applications for wagons management development.',
+            },
+            skills: {
+              title: 'Skills and Knowledge',
+              quote: 'If you want to build up a large company, first build up yourself.',
+              english: 'English (B2)',
+              german: 'German (B1/2)',
+              slovak: 'Slovak (C2)',
+              cssPreprocessors: 'CSS Preprocessors',
+              uiDesign: 'UI/UX design',
+              languges: 'Language knowledge',
+              otherKnowledge: 'I have also an experience with',
+            },
+            quotes: {
+                title: 'Favorite quotes',
+            },
+            contact: {
+              title: 'Contact',
+              quote: 'Skill is fine, and genius is splendid, but the right contacts are more valuable than either.',
+              allRights: 'All rights reserved.',
             },
             commom: {
               more: 'More',
@@ -79,6 +101,8 @@ $(function () {
               title: 'Niečo o mne',
               downloadCV: 'Životopis na stiahnutie',
               clickDownload: 'Zobraziť životopis',
+              aboutProffesions: 'Po úspešnom dokončení štúdia na Fakulte informatiky a informačných technológií (FIIT STU) a zisku titulu Ing. z informačných systémov som pokračoval v prehlbovaní svojich znalostí vo sfére IT. Prvé pracovné skúsenosti som nazbieral už počas štúdia, neskôr som sa rozhodol zamerať na vývoj webových aplikácií. Najviac ma v profesnej sfére baví a zaujíma vývoj a implementácia aplikácií, najmä ich grafického rozhrania, kde sa môžem naplno angažovať. Pri práci rád spoznávam nové a progresívne technológie, ktoré zvyšujú moje možnosti.',
+              aboutMe: 'Popri informatike sa samozrejme venujem aj iným aspektom života. Milujem prechádzky v prírode, cestovanie a s tým spojené objavovanie nových miest. Vo voľnom čase sa zaujímam o históriu, zahrám si na gitare či prevetrám bicykel. Okrem toho nepohrdnem dobrou knihou, ideálne motivačnou alebo ezoterickou.',
             },
             education: {
               title: 'Moje vzdelanie',
@@ -91,6 +115,7 @@ $(function () {
             },
             jobs: {
               title: 'PRACOVNÉ SKÚSENOSTI',
+              timeFrame: '12/2020 - Doteraz',
               softwareDeveloper: 'Softvérový vývojár',
               anasoftAbout: 'Vytvorenie systému kombinujúceho BlockChain a digitálny podpis.',
               softwareArchitecture: 'Softvérová architektúra',
@@ -100,6 +125,25 @@ $(function () {
               userTest: 'Používateľské testy',
               internationTeam: 'Medzinárodný tím - ENG',
               accentureAbout: 'Vývoj webových aplikácií pre správu a manažment wagónov.',
+            },
+            skills: {
+              title: 'Znalosti a schopnosti',
+              quote: 'Ak chcete vybudovať veľký podnik, vybudujte najprv seba.',
+              english: 'Angličtina (B2)',
+              german: 'Nemčina (B1/2)',
+              slovak: 'Slovenčina (C2)',
+              cssPreprocessors: 'CSS preprocesory',
+              uiDesign: 'UI/UX dizajn',
+              languges: 'JAZYKOVÉ ZNALOSTI',
+              otherKnowledge: 'OKREM TOHO SOM PRIČUCHOL AJ K'
+            },
+            quotes: {
+                title: 'Obľúbené citáty',
+            },
+            contact: {
+              title: 'Kontakt',
+              quote: 'Zručnosť je výborná a genialita je ohromná, ale správne kontakty sú cennejšie ako jedno aj druhé.',
+              allRights: 'Všetky práva vyhradené.',
             },
             commom: {
               more: 'Viac',
